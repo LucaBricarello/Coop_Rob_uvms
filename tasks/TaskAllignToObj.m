255aa05ec192d9ddeb037b1f273b4d02914942d9
LucaBricarello <luca.bricarello@outlook.it>
2026-01-26 17:26:43 +0100

    final fix to allign to obj task

--- tasks/TaskAllignToObj.m
+++ tasks/TaskAllignToObj.m
@@ -1,10 +1,13 @@
 classdef TaskAllignToObj < Task   
     properties
-        theta;
-        n;
-        d;
+        theta
+        %n;
+        %d;
         
-        error;
+        v_d
+        v_n
+        
+        error
     end
 
     methods
@@ -17,56 +20,59 @@ classdef TaskAllignToObj < Task
             % computing vector that connects robot origin with nodule frame origin (w_lin)
             [w_ang, w_lin] = CartError(robot.wTg , robot.wTv); % where robot.wTg is the tool goal frame (so the nodule frame)
             
-            % Projecting 3d vector on xy horizontal plane
-            w_lin(3) = 0;
+            w_k_w = [0; 0; 1];
+            w_P = eye(3) - w_k_w * w_k_w';
 
-            obj.d = wRv' * w_lin;
+            w_d = w_P * w_lin;
+            obj.v_d = wRv' * w_d;
 
-            w_axisw = w_lin; % not transposed since it is already a 3x1 vector
-            v_iv = [1,0,0]';
-            w_iv = wRv * v_iv;
+            v_i_v = [1; 0; 0];
 
-            axis_world = cross(w_axisw, w_iv);
+            w_i_v = wRv * v_i_v;
 
-            sin_theta = norm(axis_world);
-            cos_theta = w_iv' * w_axisw; % scalar product
+            w_nd = w_d / norm(w_d);
+
+            w_n = cross(w_i_v, w_nd);
+
+            sin_theta = norm(w_n);
+            cos_theta = w_nd' * w_i_v;
 
             obj.theta = atan2(sin_theta, cos_theta);
 
-            if sin_theta < 1e-6
-                n_world = [1;0;0]; 
+            if sin_theta > 1e-6
+                axis_w = w_n / sin_theta; 
             else
-                n_world = axis_world / sin_theta;
+                axis_w = [0; 0; 1]; % Asse fittizio per evitare NaN, tanto theta è 0
             end
+            
+            % Portiamo l'asse unitario nel frame veicolo
+            obj.v_n = wRv' * axis_w;
 
-            obj.n = wRv' * n_world;
+            %obj.v_n = wRv' * w_n;
 
-            %obj.xdotbar = - 0.3 *(rad_ref*obj.n - obj.theta*obj.n);
-            obj.xdotbar = - 0.3 *(rad_ref - obj.theta);
+            obj.xdotbar = - 0.3 * (obj.theta * obj.v_n);
+            %obj.xdotbar = - 0.4 * obj.theta;
 
-            obj.xdotbar = Saturate(obj.xdotbar, 0.1);
+            obj.xdotbar = Saturate(obj.xdotbar, 0.15);
 
-            obj.error = rad_ref - obj.theta;
+            obj.error = norm(obj.theta * obj.v_n);
+            %obj.error = obj.theta;
+            disp(obj.error)
         end
 
         function updateJacobian(obj, robot)
 
-            %obj.J = [zeros(1,7), zeros(1,3), obj.n'*eye(3)];
+            %obj.J = obj.v_n' * [zeros(3,7), -(1/norm(obj.v_d)^2)*skew(obj.v_d), -eye(3)];
 
-            % since n is in the world frame its orientation does not depend
-            % on the orientation of the robot
-            obj.J = obj.n' * [zeros(3,7), -(1/norm(obj.d)^2)*skew(obj.d), zeros(3,2), [0;0;-1]];
-
-            %obj.J = (obj.n * obj.n' - (obj.theta/sin(obj.theta)) * skew([1;0;0]) * skew(obj.d/norm(obj.d)) * (eye(3) - (obj.n * obj.n'))) * [zeros(3,7), -(1/norm(obj.d)^2)*skew(obj.d), -eye(3)];
-            %obj.J = (obj.n * obj.n' - (obj.theta/sin(obj.theta)) * skew([1;0;0]) * skew(obj.d/norm(obj.d)) * (eye(3) - (obj.n * obj.n'))) * [zeros(3,7), -(1/norm(obj.d)^2)*skew(obj.d), zeros(3,2), [0;0;-1]];
+            obj.J = (obj.v_n * obj.v_n' - (obj.theta/sin(obj.theta)) * skew([1;0;0]) * skew(obj.v_d/norm(obj.v_d)) * (eye(3) - (obj.v_n * obj.v_n'))) * [zeros(3,7), -(1/norm(obj.v_d)^2)*skew(obj.v_d), -eye(3)];
 
         end
         
         function updateActivation(obj, robot)
 
-            obj.A = 1;
+            %obj.A = 1;
 
-            %obj.A = eye(3);
+            obj.A = eye(3);
 
         end
     end
